727c0af2b1d80be47e223dcf66ae2211aa988cc9
Paul1 <ana_mendes@sme.prefeitura.sp.gov.br>
2021-12-08 20:50:58 -0300

    primeiro commit

---
+++ pecas.js
@@ -0,0 +1,16 @@
+var listaDePeca = ["amortecedor", "filtro de ar", "motor"]
+
+if(listaDePeca.length <10) {
+    //é possível cadastrar 
+    console.log("É possível cadastrar mais peças")
+}else {
+    console.log("Não há mais espaço na lista")
+}
+
+let peso = 200;
+if (peso <100) {
+    console.log("A peça deve pesar no mínimo 100 gramas")
+} else {
+    console.log("A peça possui peso adequado")
+}
+ 
