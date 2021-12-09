2958ef2ef0e1ef2a1c8eef439eeefe4455f7fb2c
Paul1 <ana_mendes@sme.prefeitura.sp.gov.br>
2021-12-08 21:28:34 -0300

    segundo commit

--- pecas.js
+++ pecas.js
@@ -7,10 +7,18 @@ if(listaDePeca.length <10) {
     console.log("Não há mais espaço na lista")
 }
 
-let peso = 200;
-if (peso <100) {
+let peso = 50;
+if (peso <100){
     console.log("A peça deve pesar no mínimo 100 gramas")
 } else {
     console.log("A peça possui peso adequado")
 }
  
+let nomePeca = "Disco de freio"
+if (nomePeca.length>3){
+    console.log("O nome da peça está adequado para o cadastro")
+}else{
+    console.log("O nome deve ter mais de três caracteres, digite um nome adequado")
+}
+
+
